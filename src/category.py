2d7b9019cc710ff3c20db88426bc965e6b521f51
Татьяна Бакина <trusnsk93@gmail.com>
2026-06-12 11:51:08 +0400

    Добавлены приватные атрибуты, геттеры, сеттеры, класс-метод

--- src/category.py
+++ src/category.py
@@ -3,19 +3,31 @@ class Category:
 
     name: str
     description: str
-    products: list
 
     # Атрибуты класса
     category_count: int = 0
     product_count: int = 0
 
     def __init__(self, name, description, products=None):
-        """ "Метод для инициализации экземпляра класса"""
+        """Метод для инициализации экземпляра класса"""
 
         self.name = name
         self.description = description
-        self.products = products if products else []
+        self.__products = products if products else []
 
         # Автоматически обновляем атрибуты класса
         Category.category_count += 1
-        Category.product_count += len(self.products)
+        Category.product_count += len(self.__products)
+
+    def add_product(self, product):
+        """Метод для добавления продукта в список продуктов"""
+
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self):
+        result = ""
+        for product in self.__products:
+            result += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return result
