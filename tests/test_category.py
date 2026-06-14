072beb6f45bebd6b693080d7ff827507c5b780b5
Татьяна Бакина <trusnsk93@gmail.com>
2026-06-14 09:05:20 +0400

    добавлено тестирование нового функционала и отчет о покрытии

--- tests/test_category.py
+++ tests/test_category.py
@@ -71,3 +71,18 @@ class TestCategory:
         assert "Мышь, 1000 руб. Остаток: 10 шт.\n" in result
         # Проверяем, что есть перенос строки между товарами
         assert "\n" in result
+
+    def test_category_str(self):
+        product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+        product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+        product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+
+        category = Category(
+            "Смартфоны",
+            "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+            [product1, product2, product3],
+        )
+
+        str_category = "Смартфоны, количество продуктов: 27 шт."
+
+        assert str(category) == str_category
--- tests/test_product.py
+++ tests/test_product.py
@@ -110,3 +110,19 @@ class TestProductClassMethod:
 
         assert result.name == '55" QLED 4K'
         assert result.quantity == 7
+
+    def test_product_str(self):
+        """Проверка предоставления продукта в строковом виде"""
+        product = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+        str_product = "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт."
+        assert str(product) == str_product
+
+    def test_product_add(self):
+        "Проверка сложения суммы товаров"
+
+        product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+        product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+
+        result = product1 + product2
+
+        assert result == 180000.0 * 5 + 210000.0 * 8
